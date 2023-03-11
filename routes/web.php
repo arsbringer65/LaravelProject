f997af5cee909580dbd052a2101be3fafa1f5b77
Abdul Rauf Sultan <81006767+arsbringer65@users.noreply.github.com>
2023-03-11 18:36:46 +0800

    adding login views and routes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a goup which
 | contains the "web" middleware group. Now create something great!
 |
 */
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+
